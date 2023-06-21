a43ab3fac075535133be50232bb11ba82290a1a4
cklewar <cklewar@t-online.de>
2023-06-22 01:36:06 +0200

    init

---
+++ src/bin/main.rs
@@ -0,0 +1,311 @@
+//! F5 XC regression test CI pipeline file generator.
+//! Provides command line tool to generate Gitlab CI pipeline file.
+//!
+//! Consumes input from regression configuration file provided as command line argument.
+//! Template file relays on tool provided data structure to render stage, job or variables sections.
+//! Tool supports direct rendering of given template file or generates JSON output which could be
+//! used as input for another program or workflow.
+//!
+//! Supported command line arguments:
+//! -
+//! -
+//! -
+//!
+
+
+
+use std::env;
+
+pub mod regression {
+    use std::collections::{HashSet, HashMap};
+    use std::io::Write;
+    use serde_derive::{Deserialize, Serialize};
+    use serde_json::json;
+    use tera::Tera;
+
+    const CONFIG_FILE_NAME: &str = "config.json";
+    const PIPELINE_TEMPLATE_FILE_NAME: &str = ".gitlab-ci.yml.tpl";
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionCommonConfig {
+        templates: String,
+        root_path: String,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionCiVariablesConfig {
+        name: String,
+        value: String,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionCiConfig {
+        variables: Vec<RegressionCiVariablesConfig>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionEutRunTestVerificationsConfig {
+        name: String,
+        module: String,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionEutRunTestsConfig {
+        name: String,
+        module: String,
+        verifications: Vec<RegressionEutRunTestVerificationsConfig>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionEutProviderConfig {
+        tests: Vec<RegressionEutRunTestsConfig>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionEutConfig {
+        name: String,
+        path: String,
+        stages: Vec<String>,
+        provider: HashMap<String, RegressionEutProviderConfig>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionRteConfig {
+        path: String,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionTests {
+        path: String,
+        stages: Vec<String>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RegressionVerifications {
+        path: String,
+        stages: Vec<String>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    pub struct RegressionConfig {
+        ci: RegressionCiConfig,
+        eut: RegressionEutConfig,
+        rte: RegressionRteConfig,
+        tests: RegressionTests,
+        common: RegressionCommonConfig,
+        verifications: RegressionVerifications,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RteConfigScripts {
+        name: String,
+        value: Vec<String>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct RteConfig {
+        name: String,
+        stages: Vec<String>,
+        scripts: Vec<RteConfigScripts>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct TestModuleVerificationConfig {
+        name: String,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct TestModuleRteConfig {
+        name: String,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct TestModuleConfig {
+        name: String,
+        rte: TestModuleRteConfig,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct TestModuleAndVerificationConfig {
+        name: String,
+        module: String,
+        rte: TestModuleRteConfig,
+        stage: String,
+        verifications: Vec<TestModuleVerificationConfig>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct EnvironmentProvider {
+        tmvc: Vec<TestModuleAndVerificationConfig>,
+        rtes: Vec<RteConfig>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    struct EnvironmentCi {
+        stages: Vec<String>,
+    }
+
+    #[derive(Deserialize, Serialize, Debug)]
+    pub struct Environment {
+        rc: RegressionConfig,
+        ci: EnvironmentCi,
+        providers: HashMap<String, EnvironmentProvider>,
+    }
+
+    impl Environment {
+        pub fn render(&self) -> String {
+            println!("Render regression pipeline file...");
+            let mut _tera = Tera::new(&self.rc.common.templates).unwrap();
+            let mut context = tera::Context::new();
+            context.insert("rc", &self.rc);
+            context.insert("ci", &self.ci);
+            context.insert("providers", &self.providers);
+            let rendered = _tera.render(PIPELINE_TEMPLATE_FILE_NAME, &context).unwrap();
+            println!("Render regression pipeline file -> Done.");
+            rendered
+        }
+
+        pub fn to_json(&self) -> String {
+            let j = json!({
+                "rc": &self.rc,
+                "ci": &self.ci,
+                "providers": &self.providers,
+            });
+            j.to_string()
+        }
+
+        pub fn to_file(&self, file: String) {
+            let mut f = std::fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .create(true)
+                .open(file)
+                .expect("Couldn't open file");
+            f.write_all(&self.render().as_bytes()).expect("panic while writing to file");
+        }
+    }
+
+    impl RegressionConfig {
+        fn load_regression_config(file: String) -> RegressionConfig {
+            println!("Loading regression configuration data...");
+            let data: String = String::from(file);
+            let raw = std::fs::read_to_string(&data).unwrap();
+            let cfg = serde_json::from_str::<RegressionConfig>(&raw).unwrap();
+            println!("Loading regression configuration data -> Done.");
+
+            println!("Render regression configuration file...");
+            let mut _tera = Tera::new("../../regression/configurations/*").unwrap();
+            let mut context = tera::Context::new();
+            context.insert("eut", &cfg.eut);
+            context.insert("rte", &cfg.rte);
+            let eutc = _tera.render("regression.json", &context).unwrap();
+            println!("Render regression configuration file -> Done.");
+
+            println!("Loading regression configuration data...");
+            let cfg = serde_json::from_str::<RegressionConfig>(&eutc).unwrap();
+            println!("Loading regression configuration data -> Done.");
+
+            cfg
+        }
+
+        fn load_test_modules_config(&self, tests: &Vec<RegressionEutRunTestsConfig>) -> Vec<TestModuleAndVerificationConfig> {
+            println!("Loading regression test modules configuration...");
+            let mut modules: Vec<TestModuleAndVerificationConfig> = Vec::new();
+
+            for test in tests.iter() {
+                let mut vmc: Vec<TestModuleVerificationConfig> = Vec::new();
+
+                for verification in test.verifications.iter() {
+                    let raw = std::fs::read_to_string(format!("{}/{}/{}/{}", self.common.root_path, self.verifications.path, verification.module, CONFIG_FILE_NAME)).unwrap();
+                    let cfg = serde_json::from_str::<TestModuleVerificationConfig>(&raw).unwrap();
+                    vmc.push(cfg);
+                }
+
+                let raw = std::fs::read_to_string(format!("{}/{}/{}/{}", self.common.root_path, self.tests.path, test.module, CONFIG_FILE_NAME)).unwrap();
+                let cfg = serde_json::from_str::<TestModuleConfig>(&raw).unwrap();
+
+                let tmvc = TestModuleAndVerificationConfig {
+                    rte: cfg.rte,
+                    name: test.name.clone(),
+                    stage: format!("regression-test-{}", test.name),
+                    module: cfg.name,
+                    verifications: vmc,
+                };
+
+                modules.push(tmvc);
+            }
+            println!("Loading regression test modules configuration -> Done.");
+            modules
+        }
+
+        fn load_rte_config(&self, tm_name: &String, rte_name: &String) -> RteConfig {
+            println!("Loading test module <{}> specific regression test environment configuration...", tm_name);
+            let rte_cfg = format!("{}/{}/{}/{}", self.common.root_path, self.rte.path, rte_name, CONFIG_FILE_NAME);
+            let raw = std::fs::read_to_string(rte_cfg).unwrap();
+            let cfg = serde_json::from_str::<RteConfig>(&raw).unwrap();
+            println!("Loading test module <{}> specific regression test environment configuration -> Done.", tm_name);
+            cfg
+        }
+    }
+
+    pub fn new(file: String) -> Environment {
+        println!("Loading new regression environment...");
+        let rc = RegressionConfig::load_regression_config(file);
+        let mut eps: HashMap<String, EnvironmentProvider> = HashMap::new();
+        let mut ci_stages: Vec<String> = Vec::new();
+        let mut unique_ci_stages: HashSet<String> = HashSet::new();
+
+        for (provider, config) in rc.eut.provider.iter() {
+            let mut unique_rte: HashSet<String> = HashSet::new();
+            let tmvc = rc.load_test_modules_config(&config.tests);
+            let mut rtes: Vec<RteConfig> = Vec::new();
+
+            for tm in tmvc.iter() {
+                let name = String::from(&tm.rte.name);
+                if !unique_rte.contains(&name) {
+                    unique_rte.insert(name.clone());
+                    let rte_cfg: RteConfig = rc.load_rte_config(&tm.module, &tm.rte.name);
+                    for stage in rte_cfg.stages.iter() {
+                        if !unique_ci_stages.contains(stage) {
+                            ci_stages.push(stage.clone());
+                            unique_ci_stages.insert(stage.clone());
+                        }
+                    }
+                    rtes.push(rte_cfg);
+                }
+            }
+            let _ep = EnvironmentProvider { tmvc, rtes };
+            eps.insert(provider.clone(), _ep);
+        }
+
+        for stage in rc.eut.stages.iter() {
+            ci_stages.push(stage.clone());
+        }
+        for stage in rc.tests.stages.iter() {
+            ci_stages.push(stage.clone())
+        }
+        for (_k, v) in &mut eps.iter() {
+            for tm in v.tmvc.iter(){
+                ci_stages.push(format!("regression-test-{}", tm.name.clone()));
+            }
+        }
+        for stage in rc.verifications.stages.iter() {
+            ci_stages.push(stage.clone())
+        }
+
+        let eci = EnvironmentCi { stages: ci_stages };
+        let renv = Environment { rc, ci: eci, providers: eps };
+        renv
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let config_file = String::from(&args[1]);
+    let e = regression::new(config_file);
+    // println!("{:#?}", e);
+    // println!("{}", e.to_json());
+    // println!("{}", e.render());
+    e.to_file(String::from(".gitlab-ci.yml"));
+    Ok(())
+}
---
+++ templates/.gitlab-ci.yml.tpl
@@ -0,0 +1,236 @@
+stages:
+  {% for stage in ci.stages -%}
+  - {{ stage }}
+  {% endfor %}
+variables:
+  {% for variable in rc.ci.variables -%}
+  {{ variable.name | upper }}: "{{ variable.value }}"
+  {% endfor -%}
+  {% for provider, tmvc in providers -%}
+  {% for rte in tmvc.rtes -%}
+  RTE_{{ provider | upper }}_{{ rte.name | upper }}_ROOT_DIR: "${CI_PROJECT_DIR}/{{ rc.rte.path }}/{{ rte.name }}"
+  RTE_{{ provider | upper }}_{{ rte.name | upper }}_ROOT_TF_VAR_FILE: "${RTE_{{ rte.name | upper }}_ROOT_DIR}/terraform.tfvars.json"
+  RTE_{{ provider | upper }}_{{ rte.name | upper }}_COMMON_TF_VAR_FILE: "${ARTIFACTS_ROOT_DIR}/rte_{{ rte.name }}_common.tfvars.json"
+  RTE_{{ provider | upper }}_{{ rte.name | upper }}_ARTIFACTS_FILE: "${ARTIFACTS_ROOT_DIR}/{{ provider }}/{{ rte.name }}/artifacts.tfvars"
+  {% endfor -%}
+  {% endfor %}
+deploy_rules:
+  rules:
+    - if: $ACTION == "deploy" && $CI_PIPELINE_SOURCE == "trigger" && $CI_PIPELINE_TRIGGERED == "true"
+    - if: $ACTION == "deploy" && $CI_PIPELINE_SOURCE == "web" && $CI_PIPELINE_TRIGGERED == "true"
+
+.destroy_rules:
+  rules:
+    - if: $ACTION == "destroy" && $CI_PIPELINE_SOURCE == "trigger" && $CI_PIPELINE_TRIGGERED == "true"
+    - if: $ACTION == "destroy" && $CI_PIPELINE_SOURCE == "web" && $CI_PIPELINE_TRIGGERED == "true"
+
+.regression_test_rules:
+  rules:
+    - if: $ACTION == "test" && $CI_PIPELINE_SOURCE == "trigger" && $CI_PIPELINE_TRIGGERED == "true"
+    - if: $ACTION == "test" && $CI_PIPELINE_SOURCE == "web" && $CI_PIPELINE_TRIGGERED == "true"
+
+.regression_test_verify_rules:
+  rules:
+    - if: $ACTION == "test" && $CI_PIPELINE_SOURCE == "trigger" && $CI_PIPELINE_TRIGGERED == "true"
+    - if: $ACTION == "test" && $CI_PIPELINE_SOURCE == "web" && $CI_PIPELINE_TRIGGERED == "true"
+{% for provider, values in providers -%}
+{% for tm in values.tmvc %}
+.regression_test_{{ tm.name | replace(from="-", to="_") }}:
+  rules:
+    - if: $ACTION == "test" && $CI_PIPELINE_SOURCE == "trigger" && $CI_PIPELINE_TRIGGERED == "true"
+    - if: $ACTION == "test" && $CI_PIPELINE_SOURCE == "web" && $CI_PIPELINE_TRIGGERED == "true"
+{% endfor -%}
+{% endfor %}
+base: &base
+  tags:
+    - kubernetes
+  cache:
+    policy: pull
+    key: "${CI_COMMIT_SHA}"
+  image: volterra.azurecr.io/ves.io/acmecorp-features:1.3.3
+  variables:
+    TF_VAR_feature: $FEATURE
+    TF_VAR_environment: $ENVIRONMENT
+    TF_VAR_gcp_project_id: $GCP_PROJECT_ID
+    TF_VAR_ssh_private_key_file: $RTE_SSH_PRIVATE_KEY_FILE
+    TF_VAR_ssh_public_key_file: $RTE_SSH_PUBLIC_KEY_FILE
+  before_script:
+    - |
+      #!/usr/bin/env bash
+      aws s3 cp ${SSH_PUBLIC_KEY_FILE} /
+      aws s3 cp ${SSH_PRIVATE_KEY_FILE} /
+      export TF_VAR_f5xc_api_p12_file="/${P12_FILE}"
+      aws s3 cp "${P12_FILE_PATH}/${P12_FILE}" /
+      if [ "$ENVIRONMENT" == "production" ]; then
+        export TF_VAR_f5xc_api_token=$PRODUCTION_API_TOKEN  
+      elif [ "$ENVIRONMENT" == "staging" ]; then
+        export TF_VAR_f5xc_api_token=$STAGING_API_TOKEN
+      fi
+    - terraform version
+    - echo $CI_PROJECT_DIR
+    - cd $CI_PROJECT_DIR
+{% for provider, values in providers -%}
+{% for rte in values.rtes %}
+# rte - {{ provider }} - {{ rte.name | replace(from="_", to="-")}} - artifacts
+rte-{{ provider }}-{{ rte.name | replace(from="_", to="-")}}-artifacts:
+  <<: *base
+  rules:
+    - !reference [ .regression_test_rules, rules ]
+    - !reference [ .destroy_rules, rules ]
+    {%- for test in values.tmvc %}
+    - !reference [ .regression_test_{{ test.name | replace(from="-", to="_") }}, rules ]
+    {%- endfor %}
+  stage: rte-artifacts
+  script:
+    {% for script in rte.scripts -%}
+    {% if script.name == 'artifacts' -%}
+    {% for line in script.value -%}
+    {{ line }}
+    {% endfor -%}
+    {% endif -%}
+    {% endfor -%}
+    - |
+      #!/usr/bin/env bash
+      cd $RTE_ROOT_DIR/{{ provider }}
+      terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/regression/environment/{{ provider }}"
+      terraform output > $ARTIFACTS_ROOT_DIR/{{ provider }}/{{ rte.name }}/artifacts.tfvars
+  artifacts:
+    paths:
+      - $ARTIFACTS_ROOT_DIR/{{ provider }}/{{ rte.name }}/artifacts.tfvars
+  timeout: 5m
+  retry:
+    max: 1
+    when:
+      - script_failure
+      - stuck_or_timeout_failure
+      - runner_system_failure
+
+# rte - {{ provider }} - {{ rte.name | replace(from="_", to="-")}} - apply
+rte-{{ provider }}-{{ rte.name | replace(from="_", to="-")}}-apply:
+  <<: *base
+  stage: rte-apply
+  rules:
+    - !reference [ .deploy_rules, rules ]
+  script:
+    {% for script in rte.scripts -%}
+    {% if script.name == 'apply' -%}
+    {% for line in script.value -%}
+    {{ line }}
+    {% endfor -%}
+    {% endif -%}
+    {% endfor %}
+    - |
+      #!/usr/bin/env bash
+      cd $RTE_{{ provider | upper }}_CLIENT_SERVER_ROOT_DIR
+      terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/regression/environment/{{ provider }}"
+      terraform apply -var-file=$RTE_ROOT_TF_VAR_FILE -auto-approve
+      terraform output > $ARTIFACTS_ROOT_DIR/{{ provider }}/{{ rte.name }}/artifacts.tfvars
+      echo "{{ provider }}_destination_ip=$(terraform output destination_ip)" >> $RTE_{{ provider | upper }}_{{ rte.name | upper }}_COMMON_TF_VAR_FILE
+  artifacts:
+    paths:
+      - $ARTIFACTS_ROOT_DIR/
+  timeout: 30m
+  retry:
+    max: 1
+    when:
+      - script_failure
+      - stuck_or_timeout_failure
+      - runner_system_failure
+{% endfor -%}
+{% endfor %}
+# eut - apply
+eut-apply:
+  <<: *base
+  stage: eut-apply
+  rules:
+    - !reference [ .deploy_rules, rules ]
+  script:
+    - |
+      #!/usr/bin/env bash
+      {% for provider, values in providers -%}
+      cd $EUT_ROOT_DIR/{{ provider }}
+      terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/sites/{{ provider }}" 
+      terraform apply -var-file=$EUT_ROOT_TF_VAR_FILE -var-file=$EUT_ROOT_DIR/{{ provider }}/terraform.tfvars.json {% for rte in values.rtes -%}-var-file=$RTE_{{ provider | upper }}_{{ rte.name | upper }}_ARTIFACTS_FILE {% endfor -%} -auto-approve
+      terraform output > $EUT_ROOT_DIR/{{ provider }}/site.tfvars
+      {% endfor -%}
+      cd $EUT_ROOT_DIR/common
+      terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/sites/common"
+      terraform apply -var-file=$EUT_ROOT_TF_VAR_FILE -var-file=$EUT_TF_VAR_FILE {% for provider, values in providers -%}-var-file=$EUT_ROOT_DIR/{{ provider }}/site.tfvars {% endfor -%} {% for provider, values in providers -%}{% for rte in values.rtes -%}-var-file=$RTE_{{ provider | upper }}_{{ rte.name | upper }}_COMMON_TF_VAR_FILE {% endfor -%} {% endfor -%} -auto-approve
+  timeout: 1h 30m
+  retry:
+    max: 1
+    when:
+      - script_failure
+      - stuck_or_timeout_failure
+      - runner_system_failure
+{% for provider, values in providers -%}
+{% for test in values.tmvc %}
+# test - {{ provider }} - {{ test.module | replace(from="_", to="-")}} - {{ test.rte.name  | replace(from="_", to="-") }} - run
+regression-test-{{ test.name }}:
+  <<: *base
+  rules:
+    - !reference [ .regression_test_rules, rules ]
+    - !reference [ .regression_test_{{ test.name | replace(from="-", to="_") }}, rules ]
+  stage: regression-test-run
+  script:
+    - |
+      #!/usr/bin/env bash
+      cd $CI_PROJECT_DIR/{{ rc.tests.path }}/{{ test.name }}
+      terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/{{ rc.tests.path }}/{{ test.name }}"
+      terraform apply -compact-warnings -var-file=$ARTIFACTS_ROOT_DIR/{{ test.rte.name }}.tfvars -auto-approve
+  retry:
+    max: 1
+    when:
+      - script_failure
+      - stuck_or_timeout_failure
+      - runner_system_failure
+{% endfor -%}
+{% endfor %}
+# eut - destroy
+eut-destroy:
+  <<: *base
+  stage: eut-destroy
+  rules:
+    - !reference [ .destroy_rules, rules ]
+  script:
+      - |
+         #!/usr/bin/env bash
+         cd $EUT_ROOT_DIR/common
+         terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/sites/common"
+         terraform destroy -var-file=$EUT_ROOT_TF_VAR_FILE -var-file=$EUT_TF_VAR_FILE {% for provider, values in providers -%}-var-file=$EUT_ROOT_DIR/{{ provider }}/site.tfvars {% endfor -%} {% for provider, values in providers -%}{% for rte in values.rtes -%}-var-file=$RTE_{{ provider | upper }}_{{ rte.name | upper }}_COMMON_TF_VAR_FILE {% endfor -%} {% endfor -%} -auto-approve
+         {% for provider, values in providers -%}
+         cd $EUT_ROOT_DIR/{{ provider }}
+         terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/sites/{{ provider }}" 
+         terraform destroy -var-file=$EUT_ROOT_TF_VAR_FILE -var-file=$EUT_ROOT_DIR/{{ provider }}/terraform.tfvars.json {% for rte in values.rtes -%}-var-file=$RTE_{{ provider | upper }}_{{ rte.name | upper }}_ARTIFACTS_FILE {% endfor -%} -auto-approve
+         terraform output > $EUT_ROOT_DIR/{{ provider }}/site.tfvars
+         {% endfor -%}
+  timeout: 1h 30m
+  retry:
+    max: 1
+    when:
+      - script_failure
+      - stuck_or_timeout_failure
+      - runner_system_failure
+{% for provider, values in providers -%}
+{% for rte in values.rtes %}
+# rte - {{ provider }} - {{ rte.name | replace(from="_", to="-")}} - destroy
+rte-{{ provider }}-{{ rte.name | replace(from="_", to="-")}}-destroy:
+  <<: *base
+  stage: rte-destroy
+  rules:
+    - !reference [ .destroy_rules, rules ]
+  script:
+    - |
+      #!/usr/bin/env bash
+      cd $RTE_{{ provider | upper }}_CLIENT_SERVER_ROOT_DIR
+      terraform init --backend-config="key=features/$FEATURE/$ENVIRONMENT/regression/environment/{{ provider }}"
+      terraform destroy -var-file=$RTE_ROOT_TF_VAR_FILE -auto-approve
+  timeout: 30m
+  retry:
+    max: 1
+    when:
+      - script_failure
+      - stuck_or_timeout_failure
+      - runner_system_failure
+{% endfor -%}
+{% endfor %}
